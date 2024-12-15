18a968b9adb872d87e31c2e3b2f1f693dcbe2f07
SerbanDascalu <serbandascalu30@gmail.com>
2024-12-15 17:43:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,115 @@
 #include <iostream>
 #include <winbgim.h>
 #include <stdlib.h>
+#include<graphics.h>
+#include<conio.h>
 
 using namespace std;
 
 #define MAX 20
 #define FUNDAL CYAN
 
-int stanga,sus,width,height,latura, numar;
+int stanga,sus,width,height,latura, numar, ok=1;
 bool gata;
 
 int culoare[3]= {FUNDAL,LIGHTRED,YELLOW};
 
 int TablaDeJoc[MAX][MAX];
+
+
+void mutarePiesa(int jucator, int& mutarea);
+void drawButton(int x, int y, int latime, int inalt, const char* text, int culoare, int dimtext);
+void deseneazaPiesa(int linia, int coloana, int culoare);
+void mutarePiesa(int jucator);
+int castigat(int jucator, int mutarea);
+void Meniu();
+void tabla();
+void mutare();
+void desen();
+void PvP_window();
+void PvC_window();
+void bordare();
+bool vulpeincoltita(int linia2, int coloana2);
+
+int main()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+    Meniu();
+
+    return 0;
+}
+
+void drawButton(int x, int y, int latime, int inalt, const char* text, int culoare, int dimtext)
+{
+    int lat_text,inalt_text,textx,texty;
+
+    setfillstyle(SOLID_FILL,culoare);
+    rectangle(x, y, x+latime, y+inalt);
+    floodfill(x+1, y+1, WHITE);
+    settextstyle(SIMPLEX_FONT, HORIZ_DIR, dimtext);
+
+    lat_text=textwidth(const_cast<char*>(text));
+    inalt_text=textheight(const_cast<char*>(text));
+
+    textx=x+(latime-lat_text)/2;
+    texty=y+(inalt-inalt_text)/2;
+    outtextxy(textx, texty, const_cast<char*>(text));
+}
+
+
+void Meniu()
+{
+    int xbs,ybp,ybc,latbut,inaltbut,ybe;
+
+    int screenx=850;
+    int screeny=850;
+
+    initwindow(screenx,screeny);
+
+    xbs=screenx/3;
+    latbut=200;
+    inaltbut=100;
+    ybp=screeny/5;
+    ybc=screeny/4+inaltbut;
+    ybe=screeny/4+3*inaltbut-inaltbut/2;
+
+    drawButton(xbs, ybp, latbut, inaltbut, "PvP", 9, 4);
+    drawButton(xbs, ybc, latbut, inaltbut, "PvC", 5, 4);
+    drawButton(xbs, ybe, latbut, inaltbut, "Exit", 12, 4);
+
+    while (1)
+    {
+        int x=mousex();
+        int y=mousey();
+        if (ismouseclick(WM_LBUTTONDOWN))
+            {
+                clearmouseclick(WM_LBUTTONDOWN);
+
+               if (x>=xbs && x<=xbs+latbut && y>=ybp && y<=ybp+inaltbut)
+                {
+                    cleardevice();
+                    delay(0);
+                    PvP_window();
+                    break;
+                }
+
+              if (x>=xbs && x<=xbs+latbut && y>=ybc && y<=ybc+inaltbut)
+                {
+                    cleardevice();
+                    delay(0);
+                    PvC_window();
+                    break;
+                }
+              if (x>=xbs && x<=xbs+latbut && y>=ybe && y<=ybe+inaltbut)
+                {
+                  closegraph();
+                  exit(0);
+                }
+            }
+    }
+}
+
 
 void deseneazaPiesa(int linia, int coloana, int culoare)
 {
@@ -39,6 +136,7 @@ void mutarePiesa(int jucator)
     int linia1,coloana1,linia2,coloana2,x,y;
     int click=false;
     do
+    {
         if(ismouseclick(WM_LBUTTONDOWN) && !click)
         {
             click=true;
@@ -48,9 +146,15 @@ void mutarePiesa(int jucator)
             linia1=(y-sus)/latura+1;
             coloana1=(x-stanga)/latura+1;
         }
-while (!click);
+        if(TablaDeJoc[linia1][coloana1]!=jucator)
+            click=false;
+    }
+    while (!click);
+
     click=false;
+
     do
+    {
         if(ismouseclick(WM_LBUTTONDOWN) && !click)
         {
             click=true;
@@ -60,30 +164,38 @@ while (!click);
             linia2=(y-sus)/latura+1;
             coloana2=(x-stanga)/latura+1;
         }
+        if(TablaDeJoc[linia2][coloana2]!=0)
+            click=false;
+        else if(jucator==1 && (linia2!=linia1-1 || (coloana2!=coloana1+1 && coloana2!=coloana1-1)) )
+            click=false;
+        else if(jucator==2 && ((linia2!=linia1+1 && linia2!=linia1-1) || (coloana2!=coloana1+1 && coloana2!=coloana1-1)) )
+            click=false;
+            else
+                if(vulpeincoltita(linia2,coloana2)==1)
+                    {
+                        ok=0;
+                        click=false;
+                    }
+    }
     while (!click);
 
-    if(TablaDeJoc[linia1][coloana1]==jucator && TablaDeJoc[linia2][coloana2]==0)
-    {
-        if(TablaDeJoc[linia1][coloana1]==2 && linia2==linia1+1 && (coloana2==coloana1+1 || coloana2==coloana1-1) )
-        {
-            TablaDeJoc[linia1][coloana1]=0;
-            TablaDeJoc[linia2][coloana2]=jucator;
-            deseneazaPiesa(linia1,coloana1,FUNDAL);
-            deseneazaPiesa(linia2,coloana2,culoare[jucator]);
-        }
-        else if(TablaDeJoc[linia1][coloana1]==1 && (linia2==linia1+1 || linia2==linia1-1) && (coloana2==coloana1+1 || coloana2==coloana1-1))
-        {
-            TablaDeJoc[linia1][coloana1]=0;
-            TablaDeJoc[linia2][coloana2]=jucator;
-            deseneazaPiesa(linia1,coloana1,FUNDAL);
-            deseneazaPiesa(linia2,coloana2,culoare[jucator]);
-        }
-    }
+        TablaDeJoc[linia1][coloana1]=0;
+        TablaDeJoc[linia2][coloana2]=jucator;
+        deseneazaPiesa(linia1,coloana1,FUNDAL);
+        deseneazaPiesa(linia2,coloana2,culoare[jucator]);
 }
 
-bool castigat(int jucator)
+
+
+int castigat(int jucator)
 {
-    return false;
+    for(int i=1; i<=numar; i++)
+        if(TablaDeJoc[numar][i]==jucator && jucator==2)
+            return 2;
+      if(ok==0)
+        return 1;
+
+    return 0;
 }
 
 void desen()
@@ -109,6 +221,7 @@ void tabla()
     int screeny=GetSystemMetrics(SM_CYSCREEN);
     initwindow(screenx,screeny,"Cainii si vulpea");
     desen();
+    bordare();
 
     gata=false;
     for (int i=1; i<=numar; i++)
@@ -123,6 +236,7 @@ void tabla()
 
 void mutare()
 {
+
     do
     {
         mutarePiesa(1);
@@ -130,14 +244,51 @@ void mutare()
     }
     while (!castigat(1) && !castigat(2));
 
-    getch();
-    closegraph();
+    if(castigat(2)==2)
+    {
+        settextstyle(SIMPLEX_FONT,HORIZ_DIR,4);
+        outtextxy(1000,400,"VULPEA A CASTIGAT!");
+        Meniu();
+    }
+    else
+    {
+        settextstyle(SIMPLEX_FONT,HORIZ_DIR,4);
+        outtextxy(1000,400,"CAINII AU CASTIGAT!");
+        Meniu();
+    }
+
+    //getch();
+    //closegraph();
 }
 
-int main()
+
+
+void PvP_window()
 {
     tabla();
     mutare();
 
+}
+
+void PvC_window()
+{
+    tabla();
+    mutare();
+}
+
+bool vulpeincoltita(int linia2, int coloana2)
+{
+    if(TablaDeJoc[linia2+1][coloana2+1]!=0 && TablaDeJoc[linia2+1][coloana2-1]!=0 && TablaDeJoc[linia2-1][coloana2+1]!=0 && TablaDeJoc[linia2-1][coloana2-1]!=0)
+        return 1;
+
     return 0;
+}
+
+void bordare()
+{
+    for(int i=0; i<=numar+1; i++)
+        TablaDeJoc[i][0]=TablaDeJoc[i][numar+1]=-1;
+    for(int i=0; i<=numar+1; i++)
+        TablaDeJoc[0][i]=TablaDeJoc[numar+1][i]=-1;
+
 }
